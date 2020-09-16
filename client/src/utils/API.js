9e5aa1e697afecead3144977550f9c32dc5bfdc3
Evan Bero <evanbero1980@gmail.com>
2020-09-16 10:22:10 -0400

    added axios requests in API.js file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,7 +5,31 @@ const API = {
   signUp: function(userInfo) {
     console.log(userInfo)
     return axios.post("/api/signup", userInfo);
-  }
+  },
+    //Gets all events
+    getEvents: function(){
+      return axios.get('/api/events')
+    },
+    //Adds a new event
+    addEvent: function(){
+        return axios.post('/api/addevent')
+    },
+    //Updates an event with new info entered by user
+    updateEvent: function(){
+        return axios.put('/api/updateevent')
+    },
+    //Gets all address entries
+    getAddresses: function(){
+        return axios.get('/api/addresses')
+    },
+    //Adds a new address entry
+    addAddress: function(){
+        return axios.post('/api/addaddress')
+    },
+    //Updates address with new info entered by user
+    updateAddress: function(){
+        return axios.put('/api/addaddress')
+    }
 
 }
 
