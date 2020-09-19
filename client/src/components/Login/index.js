83713c43aaae3526ac7667c16d3e67145cea55ad
kikaiaku <tjscarb89@aol.com>
2020-09-19 13:38:11 -0400

    rickybobby addressfixed

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 
-function Login() {
+function Login(props) {
     return (
         <div>
             <Container>
@@ -11,7 +11,7 @@ function Login() {
                     <Form>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="email" name="email" onChange={props.handleInputChange}placeholder="Enter email" />
                             <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                             </Form.Text>
@@ -19,13 +19,13 @@ function Login() {
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password"name="password" onChange={props.handleInputChange}placeholder="Password" />
                         </Form.Group>
                         <Form.Group controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
                         </Form.Group>
                         
-                            <Button href="/Calendar">
+                            <Button onClick={()=>props.handleSubmit()}>
                                 Login
                             </Button>
                             <Button href="/Signup">
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import Login from '../components/Login';
+import API from "../utils/API"
 
 function Home(){
-    return(
-        <Login />
-    )
-}
 
+const [email,setEmail]=useState()
+const [password,setPassword]=useState()
+
+    function handleInputChange(e){
+        let name = e.target.name
+
+    switch(name){
+        case "email":
+                setEmail(e.target.value)
+            break;
+        case "password":
+               setPassword(e.target.value)
+            break;
+    }
+
+    }
+
+
+    function handleSubmit(){
+        let data={
+            email:email,password:password
+        }
+        API.loginApp(data).then(response=>{
+            console.log(response)
+            console.log("login response")
+            sessionStorage.setItem("id",response.data.id)
+
+        })
+    }
+    return(
+        <Login handleInputChange={handleInputChange} handleSubmit={handleSubmit}/>
+      
+      
+    )
+
+    }
 
 export default Home;
