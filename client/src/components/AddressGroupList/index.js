caa54b80789d6b8cfe43db6b567c3a0358de89d9
kikaiaku <tjscarb89@aol.com>
2020-09-26 17:05:37 -0400

    checkboxes added and reworked the group page

---
+++ client/src/components/AddressGroupList/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Container, Form, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import API from '../../utils/API';
+
+// import NewAddressForm from '../NewAddressForm';
+// import AddressBook from '../../pages/AddressBook';
+
+function AddressGroupList({addressData}) {
+
+  return (
+    <div>
+{
+                addressData.map((address) => (
+ 
+        <Row>
+        
+    <Col> <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+    </InputGroup.Prepend>
+                  
+  </InputGroup>Add to Group</Col>
+    <Col>{address.firstName} {address.middleInitial} {address.lastName}</Col>
+    <Col>{address.address}, {address.city}, {address.state}, {address.zipCode}</Col>
+  </Row>
+        
+                )
+              
+                )}        
+
+                
+    //add edit button and delete button
+    </div>
+  )
+}
+
+
+
+export default AddressGroupList;
